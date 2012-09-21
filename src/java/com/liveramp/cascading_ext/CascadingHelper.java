9b4d24b8ff68a41fcc80faadb1d5e9f9a6de8432
Eddie Siegel <eddie@liveramp.com>
2012-09-21 11:32:52 -0700

    CascadingHelper is a singleton

--- src/java/com/liveramp/cascading_ext/CascadingHelper.java
+++ src/java/com/liveramp/cascading_ext/CascadingHelper.java
@@ -1,5 +1,11 @@
 package com.liveramp.cascading_ext;
 
 class CascadingHelper {
-  
+  private static final CascadingHelper instance = new CascadingHelper();
+
+  private CascadingHelper(){}
+
+  public static CascadingHelper get() {
+    return instance;
+  }
 }
