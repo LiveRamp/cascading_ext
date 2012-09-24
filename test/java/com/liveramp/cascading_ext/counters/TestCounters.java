48556364a303018af8f685d9b64628a923a53884
Eddie Siegel <eddie@liveramp.com>
2012-09-24 16:16:29 -0700

    Add TestCounters

---
+++ test/java/com/liveramp/cascading_ext/counters/TestCounters.java
@@ -0,0 +1,54 @@
+package com.liveramp.cascading_ext.counters;
+
+import cascading.flow.Flow;
+import cascading.flow.FlowProcess;
+import cascading.operation.BaseOperation;
+import cascading.operation.Filter;
+import cascading.operation.FilterCall;
+import cascading.pipe.Each;
+import cascading.pipe.Pipe;
+import cascading.scheme.hadoop.TextLine;
+import cascading.tap.SinkMode;
+import cascading.tap.Tap;
+import cascading.tap.hadoop.Hfs;
+import com.liveramp.cascading_ext.BaseTestCase;
+import com.liveramp.cascading_ext.CascadingUtil;
+import com.liveramp.cascading_ext.FileSystemHelper;
+import org.apache.hadoop.fs.Path;
+
+import java.io.IOException;
+
+/**
+ * @author eddie
+ */
+public class TestCounters extends BaseTestCase {
+  public void testBlackHoleWarning() throws IOException {
+
+    FileSystemHelper.safeMkdirs(FileSystemHelper.getFS(), new Path(getTestRoot() + "/input"));
+    Tap input = new Hfs(new TextLine(), getTestRoot() + "/input");
+    Tap output = new Hfs(new TextLine(), getTestRoot() + "/output", SinkMode.REPLACE);
+    Pipe removeAll = new Pipe("remove all");
+    removeAll = new Each(removeAll, new RemoveAll());
+
+    Flow flow = CascadingUtil.get().getFlowConnector().connect(input, output, removeAll);
+    flow.complete();
+    assertTrue("no warning on empty input", !Counters.prettyCountersString(flow).contains("BLACK HOLE WARNING"));
+
+    FileSystemHelper.createFile(FileSystemHelper.getFS(), getTestRoot() + "/input/important_messages", "hi vlad");
+
+    flow = CascadingUtil.get().getFlowConnector().connect(input, output, removeAll);
+    flow.complete();
+    assertTrue("warning on empty output and nonempty input", Counters.prettyCountersString(flow).contains("BLACK HOLE WARNING"));
+
+    flow = CascadingUtil.get().getFlowConnector().connect(input, output, new Pipe("identity"));
+    flow.complete();
+    assertTrue("no warning on nonempty output", !Counters.prettyCountersString(flow).contains("BLACK HOLE WARNING"));
+  }
+
+  @SuppressWarnings({ "rawtypes", "serial" })
+  private static class RemoveAll extends BaseOperation implements Filter {
+    public boolean isRemove(FlowProcess arg0, FilterCall arg1) {
+      return true;
+    }
+  }
+}
