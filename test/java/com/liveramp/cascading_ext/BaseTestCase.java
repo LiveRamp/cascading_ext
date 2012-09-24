96015db89190a0b07d2166038e1950de31af90ef
Eddie Siegel <eddie@liveramp.com>
2012-09-24 16:16:08 -0700

    Add getTestRoot() to BaseTestCase

--- test/java/com/liveramp/cascading_ext/BaseTestCase.java
+++ test/java/com/liveramp/cascading_ext/BaseTestCase.java
@@ -1,6 +1,8 @@
 package com.liveramp.cascading_ext;
 
 import junit.framework.TestCase;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
@@ -8,6 +10,9 @@ import org.apache.log4j.Logger;
  * @author eddie
  */
 public class BaseTestCase extends TestCase {
+  private static final String TEST_ROOT = "/tmp/cascading_ext_tests";
+  protected static final FileSystem fs = FileSystemHelper.getFS();
+
   static {
     Logger.getRootLogger().setLevel(Level.ALL);
   }
@@ -19,9 +24,20 @@ public class BaseTestCase extends TestCase {
     CascadingUtil.get().setDefaultProperty("io.sort.record.percent", 0.10);
   }
 
+  @Override
   protected void setUp() throws Exception {
     super.setUp();
     System.err.println("------ test start ------");
     System.out.println("------ test start ------");
+  }
+
+  @Override
+  public void tearDown() throws Exception {
+    super.tearDown();
+    fs.delete(new Path(TEST_ROOT), true);
+  }
+
+  protected String getTestRoot(){
+    return TEST_ROOT;
   }
 }
