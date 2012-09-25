3635bf4f96f84fb70dfc3211ff1e1bbb3a849b21
Eddie Siegel <eddie@liveramp.com>
2012-09-25 11:53:01 -0700

    Add MapSerialization

---
+++ src/java/com/liveramp/cascading_ext/serialization/MapSerialization.java
@@ -0,0 +1,47 @@
+package com.liveramp.cascading_ext.serialization;
+
+import cascading.tuple.Comparison;
+import org.apache.hadoop.io.serializer.Deserializer;
+import org.apache.hadoop.io.serializer.Serialization;
+import org.apache.hadoop.io.serializer.Serializer;
+
+import java.util.Comparator;
+import java.util.Map;
+
+public class MapSerialization implements Serialization<Map>, Comparison<Map> {
+
+  @Override
+  public Comparator<Map> getComparator(Class<Map> arg0) {
+    return new MapComparator();
+  }
+
+  @Override
+  public boolean accept(Class<?> klass) {
+    return Map.class.isAssignableFrom(klass);
+  }
+
+  @Override
+  public Deserializer<Map> getDeserializer(Class<Map> arg0) {
+    return new JavaHadoopDeserializer<Map>();
+  }
+
+  @Override
+  public Serializer<Map> getSerializer(Class<Map> arg0) {
+    return new JavaHadoopSerializer<Map>();
+  }
+
+  public Class getInstanceClass() {
+    return Map.class;
+  }
+
+  public static byte[] intToByteArray(int value) {
+    byte[] b = new byte[4];
+
+    for (int i = 0; i < 4; i++ ) {
+      int offset = (b.length - 1 - i) * 8;
+      b[i] = (byte) ((value >>> offset) & 0xFF);
+    }
+
+    return b;
+  }
+}
