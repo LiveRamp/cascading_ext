a06361bdb76461e68667ffd088829beac6f6d966
Ben Podgursky <bpodgursky@liveramp.com>
2012-10-02 11:42:50 -0700

    get rid of deprecated methods

--- src/java/com/liveramp/cascading_ext/assembly/MultiGroupBy.java
+++ src/java/com/liveramp/cascading_ext/assembly/MultiGroupBy.java
@@ -14,32 +14,6 @@ import java.util.Arrays;
 
 public class MultiGroupBy extends SubAssembly {
 
-  @Deprecated //  use constructor without pipe field sum
-  public MultiGroupBy(Pipe p0, Pipe p1, Fields groupFields, int pipeFieldsSum, MultiBuffer operation) {
-    Pipe[] pipes = new Pipe[]{p0, p1};
-    Fields[] fields = new Fields[]{groupFields, groupFields};
-    init(pipes, fields, groupFields, operation);
-  }
-
-  @Deprecated //  use constructor without pipe field sum
-  public MultiGroupBy(Pipe p0, Fields group0, Pipe p1, Fields group1, int pipeFieldsSum, Fields groupRename, MultiBuffer operation) {
-    Pipe[] pipes = new Pipe[]{p0, p1};
-    Fields[] fields = new Fields[]{group0, group1};
-    init(pipes, fields, groupRename, operation);
-  }
-
-  @Deprecated //  use constructor without pipe field sum
-  public MultiGroupBy(Pipe[] pipes, Fields groupFields, int pipeFieldsSum, MultiBuffer operation) {
-    Fields[] allGroups = new Fields[pipes.length];
-    Arrays.fill(allGroups, groupFields);
-    init(pipes, allGroups, groupFields, operation);
-  }
-
-  @Deprecated //  use constructor without pipe field sum
-  public MultiGroupBy(Pipe[] pipes, Fields[] groupFields, int pipeFieldsSum, Fields groupingRename, MultiBuffer operation) {
-    init(pipes, groupFields, groupingRename, operation);
-  }
-
   public MultiGroupBy(Pipe p0, Pipe p1, Fields groupFields, MultiBuffer operation) {
     Pipe[] pipes = new Pipe[]{p0, p1};
     Fields[] fields = new Fields[]{groupFields, groupFields};
