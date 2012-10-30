60378e1b9e1619999dbe273a4bcc555624b55dc1
Andre Rodriguez <andre@rapleaf.com>
2012-10-30 16:33:20 -0700

    various changes to how jobs within a flow are named - formatting - don't assume ids are valid paths to avoid exceptions just in case - revert structure since ids are handled by taps

--- src/java/com/liveramp/cascading_ext/flow_step_strategy/RenameJobStrategy.java
+++ src/java/com/liveramp/cascading_ext/flow_step_strategy/RenameJobStrategy.java
@@ -5,12 +5,10 @@ import cascading.flow.FlowStep;
 import cascading.flow.FlowStepStrategy;
 import cascading.tap.MultiSourceTap;
 import cascading.tap.Tap;
-import cascading.tap.hadoop.Hfs;
 import com.google.common.base.Joiner;
 import com.liveramp.cascading_ext.tap.NullTap;
 import com.twitter.maple.tap.MemorySourceTap;
 import org.apache.commons.lang.StringUtils;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapred.JobConf;
 
 import java.util.ArrayList;
@@ -21,23 +19,22 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RenameJobStrategy implements FlowStepStrategy<JobConf> {
-
   private static final int MAX_JOB_NAME_LENGTH = 175;
   private static final int MAX_SOURCE_PATH_NAMES_LENGTH = 60;
   private static final Pattern TEMP_PIPE_NAME = Pattern.compile("^(.*?)_\\d+_[A-Z0-9]{32}$");
 
-
   @Override
   public void apply(Flow<JobConf> flow, List<FlowStep<JobConf>> predecessorSteps, FlowStep<JobConf> flowStep) {
-    //  Give jobs human readable names. The default naming scheme includes a bunch of randomly generated IDs.
+    //  Give jobs human readable names. The default naming scheme includes a bunch of randomly
+    //  generated IDs.
     flowStep.getConfig().setJobName(formatJobName(flowStep));
   }
 
-  private String formatJobName(FlowStep<JobConf> flowStep) {
+  private static String formatJobName(FlowStep<JobConf> flowStep) {
     // WordCount [(2/5) input_1, input_2] -> output_1232_ABCDEF123456789...
 
     String jobName = String.format(
-        "%s [(%d/%d) %s] -> %s",
+        "%s[(%d/%d) %s] -> %s",
         flowStep.getFlowName(),
         flowStep.getStepNum(),
         flowStep.getFlow().getFlowSteps().size(),
@@ -49,59 +46,25 @@ public class RenameJobStrategy implements FlowStepStrategy<JobConf> {
     return StringUtils.abbreviate(jobName, MAX_JOB_NAME_LENGTH);
   }
 
-  protected List<String> getPathNamesFromTaps(Set<Tap> taps, boolean removeRandomSuffixFromTempTaps) {
+  private static List<String> getPathNamesFromTaps(Set<Tap> taps, boolean removeRandomSuffixFromTempTaps) {
     List<String> pathNames = new ArrayList<String>();
 
     for (Tap tap : taps) {
-      pathNames.add(getTapIdentifier(tap, removeRandomSuffixFromTempTaps));
+      if (tap instanceof NullTap || tap instanceof MemorySourceTap) {
+        // MemorySourceTap and NullTap both have really annoying random identifiers that aren't important to note
+        pathNames.add(tap.getClass().getSimpleName());
+      } else if (tap instanceof MultiSourceTap) {
+        // concatenate all sources in a multi source tap
+        Iterator<Tap> children = ((MultiSourceTap) tap).getChildTaps();
+        while (children.hasNext()) {
+          pathNames.add(getPathName(children.next(), removeRandomSuffixFromTempTaps));
+        }
+      } else {
+        pathNames.add(getPathName(tap, removeRandomSuffixFromTempTaps));
+      }
     }
 
     return pathNames;
-  }
-
-  private static String join(List<String> strings) {
-    return Joiner.on(", ").join(strings);
-  }
-
-  private static String getCanonicalName(String name) {
-    Matcher matcher = TEMP_PIPE_NAME.matcher(name);
-
-    if (matcher.matches()) {
-      return matcher.group(1);
-    } else {
-      return name;
-    }
-  }
-
-  /**
-   * Subclasses can override this and call super() to add handling for other unrecognized classes
-   *
-   * @param tap
-   * @return
-   */
-  private String getTapIdentifier(Tap tap, boolean removeRandomSuffixFromTempTaps){
-    //  MemorySourceTap and NullTap both have  really annoying random identifiers that aren't important to note
-    if (tap instanceof NullTap || tap instanceof MemorySourceTap) {
-      return tap.getClass().getSimpleName();
-    }
-
-    //  concatenate all sources in a multi source tap
-    else if (tap instanceof MultiSourceTap ) {
-      MultiSourceTap multi = (MultiSourceTap) tap;
-      List<String> sources = new ArrayList<String>();
-      Iterator children = multi.getChildTaps();
-      while (children.hasNext()) {
-        Object t = children.next();
-        if(t instanceof Tap){
-          sources.add(getTapIdentifier((Tap) t, removeRandomSuffixFromTempTaps));
-        }
-      }
-      return StringUtils.join(sources, "+");
-    }
-
-    else{
-      return getPathName(tap, removeRandomSuffixFromTempTaps);
-    }
   }
 
   private static String getPathName(Tap tap, boolean removeRandomSuffixFromTempTaps) {
@@ -110,13 +73,7 @@ public class RenameJobStrategy implements FlowStepStrategy<JobConf> {
       id = "null";
     }
 
-    String name;
-    if(tap instanceof Hfs){
-      Path path = new Path(id);
-      name = path.getName();
-    }else{
-      name = id;
-    }
+    String name = getName(id);
 
     // For temporary sources, we don't care about the random suffix appended by cascading
     if (tap.isTemporary() && removeRandomSuffixFromTempTaps) {
@@ -136,4 +93,22 @@ public class RenameJobStrategy implements FlowStepStrategy<JobConf> {
     }
   }
 
+  private static String join(List<String> strings) {
+    return Joiner.on(", ").join(strings);
+  }
+
+  private static String getCanonicalName(String name) {
+    Matcher matcher = TEMP_PIPE_NAME.matcher(name);
+
+    if (matcher.matches()) {
+      return matcher.group(1);
+    } else {
+      return name;
+    }
+  }
+
+  private static String getName(String id) {
+    String[] tokens = id.split("/");
+    return tokens[tokens.length - 1];
+  }
 }
