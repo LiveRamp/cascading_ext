9c36b557d7c18cbc6bc77246ea2471c5358c085d
Ben Podgursky <bpodgursky@liveramp.com>
2012-10-31 02:38:22 -0700

    comment

--- src/java/com/liveramp/cascading_ext/multi_group_by/CopyingIterator.java
+++ src/java/com/liveramp/cascading_ext/multi_group_by/CopyingIterator.java
@@ -21,9 +21,7 @@ import cascading.tuple.Tuple;
 import java.util.Iterator;
 
 /**
- * Decorator that creates copies of tuples returned by the decorated class. This is
- * a workaround for (what's possibly) a bug in cascading2. bpod is talking with cwensel
- * about a less hacky fix.
+ * Decorator that creates copies of tuples returned by the decorated class.
  */
 public class CopyingIterator implements Iterator<Tuple> {
   private final Iterator<Tuple> innerIterator;
