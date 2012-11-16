259e1f5d37075839fea92ff84341ec089a4d46d9
Porter Westling <pwestling@liveramp.com>
2012-11-16 11:37:06 -0800

    improved error extraction in LoggingFlow

--- src/main/java/com/liveramp/cascading_ext/flow/LoggingFlow.java
+++ src/main/java/com/liveramp/cascading_ext/flow/LoggingFlow.java
@@ -1,22 +1,52 @@
 /**
- *  Copyright 2012 LiveRamp
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Copyright 2012 LiveRamp
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.liveramp.cascading_ext.flow;
 
-import cascading.flow.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.JobID;
+import org.apache.hadoop.mapred.RunningJob;
+import org.apache.hadoop.mapred.TaskAttemptID;
+import org.apache.hadoop.mapred.TaskCompletionEvent;
+import org.apache.hadoop.mapred.TaskCompletionEvent.Status;
+import org.apache.log4j.Logger;
+
+import cascading.flow.Flow;
+import cascading.flow.FlowException;
+import cascading.flow.FlowListener;
+import cascading.flow.FlowProcess;
+import cascading.flow.FlowSkipStrategy;
+import cascading.flow.FlowStep;
+import cascading.flow.FlowStepStrategy;
 import cascading.management.UnitOfWorkSpawnStrategy;
 import cascading.stats.FlowStats;
 import cascading.stats.FlowStepStats;
@@ -24,27 +54,15 @@ import cascading.stats.hadoop.HadoopStepStats;
 import cascading.tap.Tap;
 import cascading.tuple.TupleEntryCollector;
 import cascading.tuple.TupleEntryIterator;
-import com.liveramp.cascading_ext.counters.Counters;
-import org.apache.commons.lang.StringUtils;
-import org.apache.hadoop.mapred.*;
-import org.apache.hadoop.mapred.TaskCompletionEvent.Status;
-import org.apache.log4j.Logger;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import com.liveramp.cascading_ext.counters.Counters;
 
 /**
  * Delegates actual flow operations to a flow that gets passed in, but performs some additonal logging when the job
  * completes or fails.
  */
 public class LoggingFlow implements Flow<JobConf> {
-  private static final Pattern LOG_ERROR_PATTERN = Pattern.compile("Caused by.*?more", Pattern.DOTALL);
+  private static final Pattern LOG_ERROR_PATTERN = Pattern.compile("Caused by.*?\\d\\d\\d\\d-\\d\\d-\\d\\d", Pattern.DOTALL);
   private static Logger LOG = Logger.getLogger(Flow.class);
   private static final int FAILURES_TO_QUERY = 3;
 
@@ -148,7 +166,7 @@ public class LoggingFlow implements Flow<JobConf> {
           // We limit the number of potential logs being pulled to spare the jobtracker
           if (failures.size() > 0) {
             Collections.shuffle(failures);
-            for (int i = 0; i < FAILURES_TO_QUERY; i++) {
+            for (int i = 0; i < FAILURES_TO_QUERY; i++ ) {
               jobFailures.add(getFailureLog(failures.get(i)));
             }
           }
@@ -173,13 +191,20 @@ public class LoggingFlow implements Flow<JobConf> {
     String exception = "";
     try {
       String fullLog = retrieveTaskLogs(event.getTaskAttemptId(), event.getTaskTrackerHttp());
-      Matcher matcher = LOG_ERROR_PATTERN.matcher(fullLog);
-      matcher.find();
-      exception = matcher.group();
+      exception = extractErrorFromLogString(fullLog);
     } catch (IOException e) {
       LOG.info("Regex Error!", e);
     }
     return "\nCluster Log Exception:\n" + exception;
+  }
+
+  protected static String extractErrorFromLogString(String fullLog) {
+    String exception;
+    Matcher matcher = LOG_ERROR_PATTERN.matcher(fullLog);
+    matcher.find();
+    exception = matcher.group();
+    exception.substring(0, exception.length() - 10);
+    return exception;
   }
 
   // This method pulled wholesale from a private mthod in hadoop's JobClient
