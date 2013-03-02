546cce974e6b9746244e3f26d9f1853bee672b56
Thomas Kielbus <thomas@liveramp.com>
2013-03-01 17:33:02 -0800

    Add convenience constructors to SumCounter

--- src/main/java/com/liveramp/cascading_ext/operation/SumCounter.java
+++ src/main/java/com/liveramp/cascading_ext/operation/SumCounter.java
@@ -11,17 +11,37 @@ public class SumCounter extends BaseOperation implements Filter {
   private final String fieldName;
   private final String counterGroup;
   private final String counterName;
+  private final Enum counterEnum;
+
+  public SumCounter(String fieldName, String counterName) {
+    this.fieldName = fieldName;
+    this.counterGroup = "";
+    this.counterName = counterName;
+    this.counterEnum = null;
+  }
 
   public SumCounter(String fieldName, String counterGroup, String counterName) {
     this.fieldName = fieldName;
     this.counterGroup = counterGroup;
     this.counterName = counterName;
+    this.counterEnum = null;
+  }
+
+  public SumCounter(String fieldName, Enum counter) {
+    this.fieldName = fieldName;
+    this.counterGroup = null;
+    this.counterName = null;
+    this.counterEnum = counter;
   }
 
   @Override
   public boolean isRemove(FlowProcess flowProcess, FilterCall filterCall) {
     Long value = filterCall.getArguments().getLong(fieldName);
-    flowProcess.increment(counterGroup, counterName, value);
+    if (counterEnum == null) {
+      flowProcess.increment(counterGroup, counterName, value);
+    } else {
+      flowProcess.increment(counterEnum, value);
+    }
     return false;
   }
 }
