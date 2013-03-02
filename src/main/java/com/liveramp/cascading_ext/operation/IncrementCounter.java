774560399f1f714f2c1fe766c920a8c4e41fc507
Thomas Kielbus <thomas@liveramp.com>
2013-03-01 16:45:31 -0800

    Clean up Increment and IncrementCounter

--- src/main/java/com/liveramp/cascading_ext/operation/IncrementCounter.java
+++ src/main/java/com/liveramp/cascading_ext/operation/IncrementCounter.java
@@ -1,6 +1,5 @@
 package com.liveramp.cascading_ext.operation;
 
-
 import cascading.flow.FlowProcess;
 import cascading.operation.BaseOperation;
 import cascading.operation.Filter;
@@ -8,20 +7,35 @@ import cascading.operation.FilterCall;
 
 public class IncrementCounter extends BaseOperation implements Filter {
 
-  private final String fieldName;
   private final String counterGroup;
   private final String counterName;
+  private final Enum counterEnum;
 
-  public IncrementCounter(String fieldName, String counterGroup, String counterName) {
-    this.fieldName = fieldName;
+  public IncrementCounter(String counterName) {
+    this.counterGroup = "";
+    this.counterName = counterName;
+    this.counterEnum = null;
+  }
+
+  public IncrementCounter(String counterGroup, String counterName) {
     this.counterGroup = counterGroup;
     this.counterName = counterName;
+    this.counterEnum = null;
+  }
+
+  public IncrementCounter(Enum counter) {
+    this.counterGroup = null;
+    this.counterName = null;
+    this.counterEnum = counter;
   }
 
   @Override
   public boolean isRemove(FlowProcess flowProcess, FilterCall filterCall) {
-    Long value = filterCall.getArguments().getLong(fieldName);
-    flowProcess.increment(counterGroup, counterName, value);
+    if (counterEnum == null) {
+      flowProcess.increment(counterGroup, counterName, 1);
+    } else {
+      flowProcess.increment(counterEnum, 1);
+    }
     return false;
   }
 }
--- src/main/java/com/liveramp/cascading_ext/operator/Increment.java
+++ src/main/java/com/liveramp/cascading_ext/operator/Increment.java
@@ -1,69 +1,21 @@
 package com.liveramp.cascading_ext.operator;
 
-import cascading.flow.FlowProcess;
-import cascading.operation.BaseOperation;
-import cascading.operation.Filter;
-import cascading.operation.FilterCall;
 import cascading.pipe.Each;
 import cascading.pipe.Pipe;
+import com.liveramp.cascading_ext.operation.IncrementCounter;
 
 public class Increment extends Each {
 
   public Increment(Pipe pipe, String counterName) {
-    super(pipe, new IncrementFilterString(counterName));
+    super(pipe, new IncrementCounter(counterName));
   }
 
   public Increment(Pipe pipe, String counterGroup, String counterName) {
-    super(pipe, new IncrementFilterString(counterGroup, counterName));
+    super(pipe, new IncrementCounter(counterGroup, counterName));
   }
 
   public Increment(Pipe pipe, Enum counter) {
-    super(pipe, new IncrementFilterEnum(counter));
+    super(pipe, new IncrementCounter(counter));
   }
 
-  private static abstract class IncrementFilter extends BaseOperation implements Filter {
-
-    @Override
-    public boolean isRemove(FlowProcess flowProcess, FilterCall filterCall) {
-      increment(flowProcess);
-      return false;
-    }
-
-    protected abstract void increment(FlowProcess flowProcess);
-  }
-
-  private static class IncrementFilterEnum extends IncrementFilter {
-
-    private final Enum counter;
-
-    public IncrementFilterEnum(Enum counter) {
-      this.counter = counter;
-    }
-
-    @Override
-    protected void increment(FlowProcess flowProcess) {
-      flowProcess.increment(counter, 1);
-    }
-  }
-
-  private static class IncrementFilterString extends IncrementFilter {
-
-    private final String counterGroup;
-    private final String counterName;
-
-    public IncrementFilterString(String counterName) {
-      this.counterGroup = "";
-      this.counterName = counterName;
-    }
-
-    public IncrementFilterString(String counterGroup, String counterName) {
-      this.counterGroup = counterGroup;
-      this.counterName = counterName;
-    }
-
-    @Override
-    protected void increment(FlowProcess flowProcess) {
-      flowProcess.increment(counterGroup, counterName, 1);
-    }
-  }
 }
