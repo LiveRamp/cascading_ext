30e8690dbe354ad5b940cf4535b2b9e0bfbe870e
Thomas Kielbus <thomas@liveramp.com>
2013-03-04 13:07:16 -0800

    Add TestIncrement

---
+++ src/test/java/com/liveramp/cascading_ext/assembly/TestIncrement.java
@@ -0,0 +1,44 @@
+package com.liveramp.cascading_ext.assembly;
+
+import cascading.flow.Flow;
+import cascading.operation.filter.FilterNull;
+import cascading.pipe.Each;
+import cascading.pipe.Pipe;
+import cascading.stats.FlowStats;
+import cascading.tap.Tap;
+import cascading.tuple.Fields;
+import cascading.tuple.Tuple;
+import com.liveramp.cascading_ext.BaseTestCase;
+import com.liveramp.cascading_ext.CascadingUtil;
+import com.liveramp.cascading_ext.counters.Counters;
+import com.liveramp.cascading_ext.tap.NullTap;
+import com.twitter.maple.tap.MemorySourceTap;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class TestIncrement extends BaseTestCase {
+
+  @Test
+  public void run() {
+    Tap source = new MemorySourceTap(Arrays.asList(
+        new Tuple(1),
+        new Tuple(2),
+        new Tuple((Integer) null)),
+        new Fields("field"));
+
+    Pipe pipe = new Pipe("pipe");
+    pipe = new Increment(pipe, "Group", "CounterA");
+    pipe = new Each(pipe, new FilterNull());
+    pipe = new Increment(pipe, "Group", "CounterB");
+
+    Flow f = CascadingUtil.get().getFlowConnector().connect(source, new NullTap(), pipe);
+    f.complete();
+
+    FlowStats fs = f.getFlowStats();
+
+    Assert.assertEquals(3l, Counters.get(fs, "Group", "CounterA").longValue());
+    Assert.assertEquals(2l, Counters.get(fs, "Group", "CounterB").longValue());
+  }
+}
