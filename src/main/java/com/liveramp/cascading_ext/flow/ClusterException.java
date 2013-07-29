7d30e0af8ba618f2eb6c346048abe78ab5c28d8b
Gal Topper <gal@liveramp.com>
2013-07-28 22:54:10 -0700

    Avoid repetition of cluster exception. Add dividers for clarity. Add missing newline.

---
+++ src/main/java/com/liveramp/cascading_ext/flow/ClusterException.java
@@ -0,0 +1,7 @@
+package com.liveramp.cascading_ext.flow;
+
+public class ClusterException extends RuntimeException {
+  public ClusterException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
