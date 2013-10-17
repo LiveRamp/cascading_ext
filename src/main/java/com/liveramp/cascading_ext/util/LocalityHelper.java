7e936a734e5be726caf124caae7b459186ecb5a5
Ben Podgursky <bpodgursky@gmail.com>
2013-10-17 15:56:06 -0500

    add helper method for locality and split out ranking logic

--- src/main/java/com/liveramp/cascading_ext/util/LocalityHelper.java
+++ src/main/java/com/liveramp/cascading_ext/util/LocalityHelper.java
@@ -1,5 +1,6 @@
 package com.liveramp.cascading_ext.util;
 
+import com.google.common.collect.Lists;
 import org.apache.hadoop.fs.BlockLocation;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
@@ -8,6 +9,7 @@ import org.apache.hadoop.mapred.JobConf;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -21,21 +23,32 @@ public class LocalityHelper {
   }
 
   public static String[] getHostsSortedByLocality(List<String> files, JobConf jobConf, int maxBlockLocationsPerSplit) throws IOException {
-    Map<String, Long> numBytesPerHost = new HashMap<String, Long>();
 
-    for (String file: files) {
+    List<BlockLocation> allLocations = Lists.newArrayList();
+    FileSystem fileSystem = FileSystem.get(jobConf);
+
+    for (String file : files) {
       Path path = new Path(file);
-      FileSystem fileSystem = path.getFileSystem(jobConf);
       FileStatus status = fileSystem.getFileStatus(path);
-      BlockLocation[] blockLocations = fileSystem.getFileBlockLocations(status, 0, status.getLen());
+      allLocations.addAll(Arrays.asList(fileSystem.getFileBlockLocations(status, 0, status.getLen())));
+    }
 
-      if (blockLocations != null) {
-        for (BlockLocation location: blockLocations) {
-          Long size = location.getLength();
-          for (String host: location.getHosts()) {
-            incrNumBytesPerHost(numBytesPerHost, host, size);
-          }
-        }
+    return getHostsSortedByLocalityForBlocks(allLocations, maxBlockLocationsPerSplit);
+  }
+
+  public static String[] getHostsSortedByLocality(String file, long pos, long length, JobConf jobConf, int maxBlockLocationsPerSplit) throws IOException {
+    FileSystem fs = FileSystem.get(jobConf);
+    BlockLocation[] locations = fs.getFileBlockLocations(fs.getFileStatus(new Path(file)), pos, length);
+    return getHostsSortedByLocalityForBlocks(Arrays.asList(locations), maxBlockLocationsPerSplit);
+  }
+
+  public static String[] getHostsSortedByLocalityForBlocks(List<BlockLocation> blocks, int maxBlockLocationsPerSplit) throws IOException {
+
+    Map<String, Long> numBytesPerHost = new HashMap<String, Long>();
+    for (BlockLocation location : blocks) {
+      Long size = location.getLength();
+      for (String host : location.getHosts()) {
+        incrNumBytesPerHost(numBytesPerHost, host, size);
       }
     }
 
