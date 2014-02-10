40e057783f92e26b1817e6aeaab0aec9e2060d40
Porter Westling <pwestling@liveramp.com>
2014-02-10 13:56:20 -0800

    Revert "Merge pull request #26 from LiveRamp/open_source_combiner"
    This reverts commit f8548f0a0bdc19d48843bc85b2bd202c977bcc18, reversing
    changes made to 00a3f3787e9ace324ef708c505ad58980cf1b990.

--- src/main/java/com/liveramp/cascading_ext/combiner/Evictor.java
+++
@@ -1,8 +0,0 @@
-package com.liveramp.cascading_ext.combiner;
-
-import java.io.Serializable;
-
-public interface Evictor<T> extends Serializable {
-
-  public boolean shouldEvict(T aggregate);
-}
--- src/main/java/com/liveramp/cascading_ext/combiner/ExactAggregator.java
+++
@@ -1,6 +0,0 @@
-package com.liveramp.cascading_ext.combiner;
-
-import java.io.Serializable;
-
-public interface ExactAggregator<T> extends FinalAggregator<T>, PartialAggregator<T>, Serializable {
-}
