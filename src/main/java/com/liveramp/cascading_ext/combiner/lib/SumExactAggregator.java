a4fa88d4eec37a50df2af51b5a63ac99270258a8
rhe <rhe@liveramp.com>
2015-07-17 15:02:17 -0700

    Added memory estimator for SumExactAggregator.

--- src/main/java/com/liveramp/cascading_ext/combiner/lib/SumExactAggregator.java
+++ src/main/java/com/liveramp/cascading_ext/combiner/lib/SumExactAggregator.java
@@ -1,25 +1,30 @@
 /**
- *  Copyright 2012 LiveRamp
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Copyright 2012 LiveRamp
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.liveramp.cascading_ext.combiner.lib;
 
+import java.io.Serializable;
+
 import cascading.tuple.Tuple;
 import cascading.tuple.TupleEntry;
+
 import com.liveramp.cascading_ext.combiner.ExactAggregator;
-import com.liveramp.cascading_ext.combiner.lib.BaseExactAggregator;
+import com.liveramp.commons.util.IntegerMemoryUsageEstimator;
+import com.liveramp.commons.util.LongMemoryUsageEstimator;
+import com.liveramp.commons.util.MemoryUsageEstimator;
 
 public class SumExactAggregator extends BaseExactAggregator<Number[]> implements ExactAggregator<Number[]> {
 
@@ -58,13 +63,13 @@ public class SumExactAggregator extends BaseExactAggregator<Number[]> implements
           if (aggregate[i] == null) {
             aggregate[i] = 0L;
           }
-          aggregate[i] = ((Long) aggregate[i]) + nextValue.getLong(i);
+          aggregate[i] = ((Long)aggregate[i]) + nextValue.getLong(i);
           break;
         case DOUBLE:
           if (aggregate[i] == null) {
             aggregate[i] = 0D;
           }
-          aggregate[i] = ((Double) aggregate[i]) + nextValue.getDouble(i);
+          aggregate[i] = ((Double)aggregate[i]) + nextValue.getDouble(i);
           break;
       }
     }
@@ -83,5 +88,37 @@ public class SumExactAggregator extends BaseExactAggregator<Number[]> implements
       tuple.add(value);
     }
     return tuple;
+  }
+
+  public static class SumExactAggregatorMemoryUsageEstimator implements MemoryUsageEstimator<Number[]>, Serializable {
+
+    private static final IntegerMemoryUsageEstimator imue = new IntegerMemoryUsageEstimator();
+    private static final LongMemoryUsageEstimator lmue = new LongMemoryUsageEstimator();
+
+    @Override
+    public long estimateMemorySize(Number[] items) {
+      long size = 12L;  // estimate of array overhead
+
+      for (Number number : items) {
+        if (number == null) {
+          size += 8L;
+        } else if (number instanceof Integer) {
+          size += imue.estimateMemorySize((Integer)number);
+        } else if (number instanceof Long) {
+          size += lmue.estimateMemorySize((Long)number);
+        } else if (number instanceof Double) {
+          size += 16L;  // same as Long
+        } else if (number instanceof Short) {
+          size += 8L;
+        } else if (number instanceof Float) {
+          size += 16L;  // same as Integer
+        } else if (number instanceof Byte) {
+          size += 8L;
+        } else {
+          throw new RuntimeException("Unrecognized number format: " + number.getClass());
+        }
+      }
+      return size;
+    }
   }
 }
