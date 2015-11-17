b7675e647e02312265de6c8e2e1f403d41185745
Ben Podgursky <bpodgursky@gmail.com>
2015-11-16 16:09:42 -0800

    consolidate functions to avoid duplicate byte copying

--- src/main/java/com/liveramp/cascading_ext/assembly/CreateBloomFilter.java
+++ src/main/java/com/liveramp/cascading_ext/assembly/CreateBloomFilter.java
@@ -17,20 +17,7 @@
 package com.liveramp.cascading_ext.assembly;
 
 import java.io.IOException;
-import java.util.Random;
 
-import com.clearspring.analytics.stream.cardinality.HyperLogLog;
-import org.apache.hadoop.io.BytesWritable;
-import org.apache.hadoop.mapred.JobConf;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import cascading.flow.FlowProcess;
-import cascading.flow.hadoop.HadoopFlowProcess;
-import cascading.operation.BaseOperation;
-import cascading.operation.Filter;
-import cascading.operation.FilterCall;
-import cascading.operation.OperationCall;
 import cascading.pipe.Each;
 import cascading.pipe.Every;
 import cascading.pipe.GroupBy;
@@ -38,14 +25,8 @@ import cascading.pipe.Pipe;
 import cascading.pipe.SubAssembly;
 import cascading.pipe.assembly.Unique;
 import cascading.property.ConfigDef;
-import cascading.scheme.hadoop.SequenceFile;
-import cascading.tap.hadoop.Hfs;
 import cascading.tuple.Fields;
-import cascading.tuple.Tuple;
-import cascading.tuple.TupleEntry;
-import cascading.tuple.TupleEntryCollector;
 
-import com.liveramp.cascading_ext.Bytes;
 import com.liveramp.cascading_ext.bloom.BloomProps;
 import com.liveramp.cascading_ext.bloom.operation.CreateBloomFilterFromIndices;
 import com.liveramp.cascading_ext.bloom.operation.GetIndices;
@@ -56,10 +37,7 @@ public class CreateBloomFilter extends SubAssembly {
   public CreateBloomFilter(Pipe keys, String bloomFilterID, String approxCountPartsDir, String bloomPartsDir, String keyBytesField) throws IOException {
     super(keys);
 
-    // Collect stats used to configure the bloom filter creation step
-    Pipe smallPipe = new Each(keys, new CollectKeyStats(keyBytesField));
-
-    smallPipe = new Each(smallPipe, new Fields(keyBytesField), new GetIndices(HashFunctionFactory.DEFAULT_HASH_FACTORY), new Fields("split", "index", "hash_num"));
+    Pipe smallPipe = new Each(keys, new Fields(keyBytesField), new GetIndices(HashFunctionFactory.DEFAULT_HASH_FACTORY), new Fields("split", "index", "hash_num"));
     smallPipe = new Each(smallPipe, new Fields("split", "index", "hash_num"), new Unique.FilterPartialDuplicates());
     smallPipe = new GroupBy(smallPipe, new Fields("split"));
     smallPipe = new Every(smallPipe, new Fields("index", "hash_num"), new CreateBloomFilterFromIndices(), Fields.ALL);
@@ -70,64 +48,5 @@ public class CreateBloomFilter extends SubAssembly {
     bloomDef.setProperty(BloomProps.TARGET_BLOOM_FILTER_ID, bloomFilterID);
 
     setTails(smallPipe);
-  }
-
-  /**
-   * Used to collect the following stats about the keys in a bloom join:
-   * - approximate distinct number (uses HLL)
-   * - approximate average key size
-   * - approximate average tuple size
-   */
-  private static class CollectKeyStats extends BaseOperation implements Filter {
-    private static Logger LOG = LoggerFactory.getLogger(CollectKeyStats.class);
-    private static transient Random RAND;
-
-    private transient HyperLogLog approxCounter;
-    private final String bytesField;
-
-    public CollectKeyStats(String bytesField) {
-      this.bytesField = bytesField;
-    }
-
-    @Override
-    public boolean isRemove(FlowProcess flowProcess, FilterCall filterCall) {
-      TupleEntry tuple = filterCall.getArguments();
-
-      BytesWritable key = (BytesWritable) tuple.getObject(bytesField);
-      approxCounter.offer(Bytes.getBytes(key));
-
-      return false;
-    }
-
-    @Override
-    public void prepare(FlowProcess flowProcess, OperationCall operationCall) {
-      JobConf conf = (JobConf) flowProcess.getConfigCopy();
-      approxCounter = new HyperLogLog(BloomProps.getHllErr(conf));
-    }
-
-    @Override
-    public void cleanup(FlowProcess flowProcess, OperationCall operationCall) {
-      JobConf conf = (JobConf) flowProcess.getConfigCopy();
-
-      try {
-        LOG.info("HLL counter found " + approxCounter.cardinality() + " distinct keys");
-
-        Hfs tap = new Hfs(new SequenceFile(new Fields("bytes")), BloomProps.getApproxCountsDir(conf));
-        TupleEntryCollector out = tap.openForWrite(new HadoopFlowProcess(conf));
-        out.add(new Tuple(new BytesWritable(approxCounter.getBytes())));
-        out.close();
-
-      } catch (IOException e) {
-        throw new RuntimeException("couldn't write approximate counts to side bucket", e);
-      }
-    }
-
-    protected static Random getRand() {
-      if (RAND == null) {
-        RAND = new Random();
-      }
-
-      return RAND;
-    }
   }
 }
--- src/main/java/com/liveramp/cascading_ext/bloom/operation/GetIndices.java
+++ src/main/java/com/liveramp/cascading_ext/bloom/operation/GetIndices.java
@@ -16,25 +16,41 @@
 
 package com.liveramp.cascading_ext.bloom.operation;
 
+import java.io.IOException;
+
+import com.clearspring.analytics.stream.cardinality.HyperLogLog;
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.mapred.JobConf;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import cascading.flow.FlowProcess;
+import cascading.flow.hadoop.HadoopFlowProcess;
 import cascading.operation.BaseOperation;
 import cascading.operation.Function;
 import cascading.operation.FunctionCall;
 import cascading.operation.OperationCall;
+import cascading.scheme.hadoop.SequenceFile;
+import cascading.tap.hadoop.Hfs;
 import cascading.tuple.Fields;
 import cascading.tuple.Tuple;
+import cascading.tuple.TupleEntryCollector;
+
 import com.liveramp.cascading_ext.Bytes;
 import com.liveramp.cascading_ext.bloom.BloomProps;
 import com.liveramp.cascading_ext.bloom.BloomUtil;
 import com.liveramp.cascading_ext.hash.HashFunction;
 import com.liveramp.cascading_ext.hash.HashFunctionFactory;
-import org.apache.hadoop.io.BytesWritable;
-import org.apache.hadoop.mapred.JobConf;
 
 public class GetIndices extends BaseOperation implements Function {
+  private static Logger LOG = LoggerFactory.getLogger(GetIndices.class);
+
   private long splitSize;
   private final HashFunctionFactory factory;
   private HashFunction function;
+
+  private transient HyperLogLog approxCounter;
+
 
   public GetIndices(HashFunctionFactory factory) {
     super(1, new Fields("split", "index", "hash_num"));
@@ -42,22 +58,45 @@ public class GetIndices extends BaseOperation implements Function {
   }
 
   public void prepare(FlowProcess flowProcess, OperationCall operationCall) {
-    JobConf conf = (JobConf) flowProcess.getConfigCopy();
+    JobConf conf = (JobConf)flowProcess.getConfigCopy();
 
     int maxHashes = BloomProps.getMaxBloomHashes(conf);
     long numBits = BloomProps.getNumBloomBits(conf);
     int numSplits = BloomProps.getNumSplits(conf);
     splitSize = BloomUtil.getSplitSize(numBits, numSplits);
     function = factory.getFunction(numBits, maxHashes);
+    approxCounter = new HyperLogLog(BloomProps.getHllErr(conf));
+
   }
 
   @Override
   public void operate(FlowProcess flow, FunctionCall call) {
-    byte[] bytes = Bytes.getBytes((BytesWritable) call.getArguments().getObject(0));
+    byte[] bytes = Bytes.getBytes((BytesWritable)call.getArguments().getObject(0));
+
+    approxCounter.offer(bytes);
+
     long[] h = function.hash(bytes);
     for (int i = 0; i < h.length; i++) {
       Tuple tuple = new Tuple(h[i] / splitSize, h[i] % splitSize, i);
       call.getOutputCollector().add(tuple);
     }
   }
+
+  @Override
+  public void cleanup(FlowProcess flowProcess, OperationCall operationCall) {
+    JobConf conf = (JobConf) flowProcess.getConfigCopy();
+
+    try {
+      LOG.info("HLL counter found " + approxCounter.cardinality() + " distinct keys");
+
+      Hfs tap = new Hfs(new SequenceFile(new Fields("bytes")), BloomProps.getApproxCountsDir(conf));
+      TupleEntryCollector out = tap.openForWrite(new HadoopFlowProcess(conf));
+      out.add(new Tuple(new BytesWritable(approxCounter.getBytes())));
+      out.close();
+
+    } catch (IOException e) {
+      throw new RuntimeException("couldn't write approximate counts to side bucket", e);
+    }
+  }
+
 }
