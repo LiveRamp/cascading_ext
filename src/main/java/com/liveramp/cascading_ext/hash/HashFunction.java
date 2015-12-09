4f6efc12fb4f14802bc62302f65a2ec3be610fc1
Ben Podgursky <bpodgursky@gmail.com>
2015-12-09 15:44:12 -0800

    Revert "Revert "Merge pull request #53 from LiveRamp/reduce_creation""
    This reverts commit 4c0023a7c319bddbd842cad70a00b560eb7bc695.

--- src/main/java/com/liveramp/cascading_ext/hash/HashFunction.java
+++ src/main/java/com/liveramp/cascading_ext/hash/HashFunction.java
@@ -29,11 +29,16 @@ public abstract class HashFunction implements Serializable {
   }
 
   public long[] hash(byte[] b) {
+    long[] result = new long[numHashes];
+    hash(b, result);
+    return result;
+  }
+
+    public void hash(byte[] b, long[] result) {
     if (b.length == 0) {
       throw new IllegalArgumentException("key length must be > 0");
     }
 
-    long[] result = new long[numHashes];
     int initval = -1;
     for (int i = 0; i < numHashes; i++) {
       long hash;
@@ -41,7 +46,6 @@ public abstract class HashFunction implements Serializable {
       initval = (int) hash;
       result[i] = Math.abs(hash % maxValue);
     }
-    return result;
   }
 
   public abstract long hash(byte[] data, int length, int seed);
