a2ef74e5e74291d43a57a9306c93dcfc1867fe51
Porter Westling <pwestling@liveramp.com>
2017-03-22 11:34:28 -0700

    add yarn metrics to all jobs

---
+++ src/main/java/com/liveramp/cascading_ext/yarn/YarnPerformanceMetricsHelper.java
@@ -0,0 +1,97 @@
+package com.liveramp.cascading_ext.yarn;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Optional;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.apache.commons.io.IOUtils;
+import org.apache.hadoop.mapred.JobConf;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class YarnPerformanceMetricsHelper {
+
+  public static final String YARN_STATS_GROUP = "YarnStats";
+  public static final String YARN_MEM_SECONDS_COUNTER = "MB_SECONDS";
+  public static final String YARN_VCORE_SECONDS_COUNTER = "VCORES_SECONDS";
+
+  private static Logger LOG = LoggerFactory.getLogger(YarnPerformanceMetricsHelper.class);
+
+  public static class ApplicationInfo {
+    Long mbSeconds;
+    Long vcoreSeconds;
+    String trackingUrl;
+
+    public ApplicationInfo(Long mbSeconds, Long vcoreSeconds, String trackingUrl) {
+      this.mbSeconds = mbSeconds;
+      this.vcoreSeconds = vcoreSeconds;
+      this.trackingUrl = trackingUrl;
+    }
+
+    public Long getMbSeconds() {
+      return mbSeconds;
+    }
+
+    public Long getVcoreSeconds() {
+      return vcoreSeconds;
+    }
+
+    public String getTrackingUrl() {
+      return trackingUrl;
+    }
+
+    @Override
+    public String toString() {
+      return "ApplicationInfo{" +
+          "mbSeconds=" + mbSeconds +
+          ", vcoreSeconds=" + vcoreSeconds +
+          ", trackingUrl='" + trackingUrl + '\'' +
+          '}';
+    }
+  }
+
+  private static String GETRequest(String urlString) throws IOException {
+    URL url = new URL(urlString);
+    HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+    urlConnection.setRequestMethod("GET");
+    return IOUtils.toString(urlConnection.getInputStream());
+  }
+
+  public static Optional<ApplicationInfo> getYarnAppInfo(JobConf conf, String appId) {
+    String yarnApiAddress = conf.get("yarn.resourcemanager.webapp.address", "");
+    if(!yarnApiAddress.isEmpty()) {
+      return getYarnAppInfo(yarnApiAddress, appId);
+    }else{
+      return Optional.empty();
+    }
+  }
+
+  public static Optional<ApplicationInfo> getYarnAppInfo(String yarnApiAddress, String appId) {
+    if (yarnApiAddress != null) {
+      try {
+        String urlString = "http://" + yarnApiAddress + "/ws/v1/cluster/apps/" + appId;
+        String jsonResponse = GETRequest(urlString);
+        JsonObject parsed = (JsonObject)new JsonParser().parse(jsonResponse);
+        JsonObject app = parsed.getAsJsonObject("app");
+        ApplicationInfo info = new ApplicationInfo(
+            app.get("vcoreSeconds").getAsLong(),
+            app.get("memorySeconds").getAsLong(),
+            app.get("trackingUrl").getAsString()
+        );
+        LOG.info("Got YARN info: " + info);
+        return Optional.of(info);
+      } catch (IOException e) {
+        LOG.error("Error getting yarn info:", e);
+        return Optional.empty();
+      }
+    } else {
+      LOG.error("YARN api address not set");
+      return Optional.empty();
+    }
+  }
+
+
+}
